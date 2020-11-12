41087cf1bdd42f66e76de7c46ce6b73955a24234
Amy Gamble <amymgambl@gmail.com>
2020-11-12 12:59:54 -0600

    completed through 14.3.2

--- CodingEventsDemo/Controllers/EventsController.cs
+++ CodingEventsDemo/Controllers/EventsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using CodingEventsDemo.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,7 +12,7 @@ namespace coding_events_practice.Controllers
     public class EventsController : Controller
     {
 
-        static private Dictionary<string, string> Events = new Dictionary<string, string>();
+        static private List<Event> Events = new List<Event>();
 
         // GET: /<controller>/
         public IActionResult Index()
@@ -28,9 +29,9 @@ namespace coding_events_practice.Controllers
 
         [HttpPost]
         [Route("Events/Add")]
-        public IActionResult NewEvent(string name, string desc = "")
+        public IActionResult NewEvent(string name, string desc)
         {
-            Events.Add(name, desc);
+            Events.Add(new Event(name, desc));
             
 
             return Redirect("/Events");
