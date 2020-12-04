45e20f5a50b3ccc02599732faa8e6f20335cc1de
Amy Gamble <amymgambl@gmail.com>
2020-12-04 13:26:53 -0600

    ch 19 studio event category page and form to add new categories

--- CodingEventsDemo/Controllers/EventCategoryController.cs
+++ CodingEventsDemo/Controllers/EventCategoryController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using CodingEventsDemo.Data;
 using CodingEventsDemo.Models;
+using CodingEventsDemo.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -26,6 +27,36 @@ namespace CodingEventsDemo.Controllers
             ViewBag.title = "All Categories";
             List<EventCategory> categories = context.Categories.ToList();
             return View(categories);
+        }
+
+        [HttpGet]
+        [Route("EventCategory/Create")]
+        public IActionResult Create()
+        {
+            AddEventCategoryViewModels addEventCategoryViewModels = new AddEventCategoryViewModels();
+
+            return View();
+        }
+
+        [HttpPost]
+        [Route("EventCategory/CreateForm")]
+
+        public IActionResult ProcessCreateEventCategoryForm(AddEventCategoryViewModels addEventCategoryViewModels)
+        {
+            if(ModelState.IsValid)
+            {
+                EventCategory newEventCategory = new EventCategory
+                {
+                    Name = addEventCategoryViewModels.Name,
+                };
+
+                context.Categories.Add(newEventCategory);
+                context.SaveChanges();
+
+                return Redirect("/EventCategory");
+            }
+
+            return View("Create", addEventCategoryViewModels);
         }
     }
 }
