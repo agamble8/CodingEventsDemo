4a0b9f59cd61edb4a745dadce4c6c4301fbe593e
Amy Gamble <amymgambl@gmail.com>
2020-12-17 19:21:29 -0600

    ch22 studio with start of scaffolding

--- CodingEventsDemo/Migrations/20201215214953_AddingIdentityMigration.Designer.cs
+++ CodingEventsDemo/Migrations/20201215214953_AddingIdentityMigration.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using CodingEventsDemo.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CodingEventsDemo.Migrations
 {
     [DbContext(typeof(EventDbContext))]
-    partial class EventDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201215214953_AddingIdentityMigration")]
+    partial class AddingIdentityMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
